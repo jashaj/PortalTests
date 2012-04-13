45aef18f64a951ae78e897d7f8e42087dc150f5d
Jasha Joachimsthal <jasha@jasha.eu>
2012-04-13 12:22:23 +0200

    Added jbehave skeleton

---
+++ src/main/java/eu/jasha/portaltests/steps/LifecycleSteps.java
@@ -0,0 +1,11 @@
+package eu.jasha.portaltests.steps;
+
+import org.jbehave.web.selenium.PerStoryWebDriverSteps;
+import org.jbehave.web.selenium.WebDriverProvider;
+
+public class LifecycleSteps extends PerStoryWebDriverSteps {
+
+    public LifecycleSteps(WebDriverProvider webDriverProvider) {
+        super(webDriverProvider);
+    }
+}
