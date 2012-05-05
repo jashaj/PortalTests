10fddeb4280258c26d99a788b6505740d791171c
Jasha Joachimsthal <jasha@jasha.eu>
2012-05-05 11:31:10 +0200

    Make batch file / shell script obsolete - Use antrun plugin to delete database - Use <inherited>false</inherited> to only trigger delete, cargo start/stop in the parent module

--- run-itests.bat
+++
@@ -1,6 +0,0 @@
-echo 'Cleaning resources from previous runs...'
-del \tmp\rave_db.*
-echo 'Start integration tests...'
-mvn -e clean integration-test
-echo '...End integration tests'
-
--- run-itests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo 'Cleaning resources from previous runs...'
-rm -f /tmp/rave_db.*
-echo 'Start integration tests...'
-mvn -e clean integration-test
-echo '...End integration tests'
-
